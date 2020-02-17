354db154b98493114ba801ccf8bf88a3f8e0621a
panda7802 <814252020@qq.com>
2020-02-17 17:22:55 +0800

    增加单省统计

--- ncov2019/run_sql.sql
+++ ncov2019/run_sql.sql
@@ -20,3 +20,22 @@ order by "确诊率" desc
 --order by 死亡人数 desc
 --order by 治愈率 desc, 确诊人数 asc
 --order by 死亡率 desc , 确诊人数 desc
+
+
+
+
+select c.name,
+           (a.confirmedNum - b.confirmedNum) as '新增确诊',
+           (a.curesNum - b.curesNum) as '新增治愈',
+           (a.deathsNum - b.deathsNum) as '新增死亡',
+           a.s_date,
+           a.*
+    from ncov_cnovhisinfo a,
+         ncov_cnovhisinfo b,
+         ncov_zoneinfo c
+    where date(a.s_date, 'start of day', '-1 days') = b.s_date
+      and a.pid = b.pid
+      and a.pid = c.mid
+      and a.pid in ('32','31')
+      and b.s_date >= (select min(s_date) from ncov_cnovhisinfo group by pid and pid in ('32','31') order by s_date desc limit 1)
+    order by a.pid, a.s_date
